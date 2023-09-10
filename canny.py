78331dcb167c2e7ea83b66abb00d0ee963bdb2d3
Sailavanya Narthu <98532973+SaiLavanya1@users.noreply.github.com>
2023-09-10 16:13:54 -0500

    Update canny.py

--- canny.py
+++ canny.py
@@ -2,8 +2,8 @@ def is_local_max(E_mag, i, j, s_theta, t_low):
     mC = E_mag[i,j] 
     if mC < t_low: 
        return False
-else:
-    mL = -1
+    else:
+     mL = -1
     if s_theta == 0:
         mL = E_mag[i, j-1]
     elif s_theta == 1:
@@ -24,4 +24,4 @@ else:
     elif s_theta == 3:
         mL = E_mag[i+1, j-1]
 
-return mL <= mC and mC >= mR
+    return mL <= mC and mC >= mR
